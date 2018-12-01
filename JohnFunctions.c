8847116d8da33a7b62d98fd6c597f83b60388d13
JohnHickox <44852836+JohnHickox@users.noreply.github.com>
2018-11-30 16:51:53 -0800

    Add files via upload
    11/30/2018 Added functions, changed auton

---
+++ JohnFunctions.c
@@ -0,0 +1,62 @@
+void haultYo (){
+	motor[port1]=0;
+	motor[port2]=0;
+	motor[port3]=0;
+	motor[port4]=0;
+	motor[port5]=0;
+	motor[port6]=0;
+	motor[port7]=0;
+	motor[port8]=0;
+	motor[port9]=0;
+	motor[port10]=0;
+}
+
+
+void goFoward (int pwr){
+		motor[port4] = -pwr;
+		motor[port3] = -pwr;
+		motor[port2] = pwr;
+		motor[port1] = pwr;
+	}
+
+	void goBackward (int pwr)
+	{
+		motor[port4] = pwr;
+		motor[port3] = pwr;
+		motor[port2] = -pwr;
+		motor[port1] = -pwr;
+	}
+	void lTurn (int pwr)
+	{
+		motor[port4] = -pwr;
+		motor[port3] = -pwr;
+		motor[port2] = -pwr;
+		motor[port1] = -pwr;
+	}
+	void rTurn (int pwr){
+		motor[port4] = pwr;
+		motor[port3] = pwr;
+		motor[port2] = pwr;
+		motor[port1] = pwr;
+	}
+
+	void flyEnable(int pwr){
+	motor[port6] = -79;
+	motor[port5] = -79;
+
+	}
+
+	void intakeEnable(int pwr){
+	motor[port9] = -pwr;
+	motor[port10] = -pwr;
+}
+void flyDisable(){
+	motor[port6] = 0;
+	motor[port5] = 0;
+
+	}
+
+	void intakeEnable(){
+	motor[port9] = 0;
+	motor[port10] = 0;
+}
